da0831dfd014db67dbf5f3c9a76b183268e45622
svandecappelle <steeve.vandecappelle@opti-mix.fr>
2015-02-23 14:28:31 +0100

    javadoc on Query

--- src/main/java/com/mizore/sql/qmaker/utils/QueryFactory.java
+++ src/main/java/com/mizore/sql/qmaker/utils/QueryFactory.java
@@ -6,8 +6,28 @@ import com.mizore.sql.qmaker.query.Field;
 import com.mizore.sql.qmaker.query.From;
 import com.mizore.sql.qmaker.query.SqlRestriction;
 
+/**
+ * @author svandecappelle
+ *
+ * @since 0.0.1
+ *
+ *        Query string factory builder.
+ */
 public final class QueryFactory {
 
+    /**
+     * Private constructor to disable instanciation.
+     */
+    private QueryFactory() {
+    }
+
+    /**
+     * Build Select clause from fields.
+     * 
+     * @param fields
+     *            fields to select.
+     * @return the SQL String representation of select clause.
+     */
     public static String buildSelect(Collection<Field> fields) {
 
         StringBuilder buffer = new StringBuilder();
@@ -27,6 +47,13 @@ public final class QueryFactory {
         return buffer.toString();
     }
 
+    /**
+     * Build from clause.
+     * 
+     * @param fromClause
+     *            the from clause.
+     * @return the SQL String representation of From clause.
+     */
     public static String buildFrom(From fromClause) {
         StringBuilder buffer = new StringBuilder();
 
@@ -37,6 +64,13 @@ public final class QueryFactory {
         return buffer.toString();
     }
 
+    /**
+     * Build where SQL restrictions clauses.
+     * 
+     * @param whereClauses
+     *            the restrictions to builds.
+     * @return the SQL String representation of Where restrictions clauses.
+     */
     public static String buildWhere(Collection<SqlRestriction> whereClauses) {
         StringBuilder buffer = new StringBuilder();
 
